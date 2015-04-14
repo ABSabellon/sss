c3365165786b7225115135856367007f6f3d9f81
viciouzkitten <aileen.sabellon@gmail.com>
2015-04-14 23:30:28 +0800

    check out mo.. wala pang db yan

--- application/views/modal/reference_check_modal.php
+++ application/views/modal/reference_check_modal.php
@@ -4,11 +4,27 @@
 			<div class="modal-header">
 				 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
 				<h4 class="modal-title" id="myModalLabel">
-					Modal title
+					ENTER YOUR REFERENCE NUMBER HERE
 				</h4>
 			</div>
 			<div class="modal-body">
-				...
+				<div class="row form-group">
+						
+									
+						<?php
+							//first name
+							$new_refno_input = array(
+								'name' => 'new_refno',
+								'id' => 'new_refno',
+								'class' => 'form-control',
+								'placeholder' => 'Reference Number',
+								'required' => 'true',
+								'readonly' => 'readyonly',
+								'value' => $new_refno
+							);
+							echo "<div class='col-xs-4 col-md-4'>" . form_input($new_refno_input) . "</div>";
+							?>
+							</div>
 			</div>
 			<div class="modal-footer">
 				 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save changes</button>
